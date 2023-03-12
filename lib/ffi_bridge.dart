9462e2a0364a9f0a2a7750576388da6e4bb30212
Iaroslav Dochien <iaroslav@atomos.com>
2023-03-13 09:15:47 +1100

    Set up interop in dart

--- lib/ffi_bridge.dart
+++ lib/ffi_bridge.dart
@@ -1,4 +1,5 @@
 import 'dart:ffi';
+import 'dart:isolate';
 import 'package:ffi/ffi.dart';
 
 class FFIBridge {
@@ -6,6 +7,21 @@ class FFIBridge {
         nativeNdiMonitorLib = (DynamicLibrary.open('libndi-monitor.so')); // android & linux
         final scanNdiSources_ = nativeNdiMonitorLib.lookup<NativeFunction<Int32 Function()>>('scanNdiSources');
         scanNdiSources = scanNdiSources_.asFunction<int Function()>();
+
+        // initialize the native dart API
+        final initializeApi = nativeNdiMonitorLib.lookupFunction<IntPtr Function(Pointer<Void>),
+            int Function(Pointer<Void>)>("initializeApiDLData");
+        var initRet = initializeApi(NativeApi.initializeApiDLData);
+        if (initRet != 0) {
+          throw "Failed to initialize Dart API: $initRet";
+        }
+
+        final interactiveCppRequests = ReceivePort()
+            ..listen((message) { });
+
+        final int nativePort = interactiveCppRequests.sendPort.nativePort;
+        final void Function(int port) setDartApiMessagePort = nativeNdiMonitorLib.lookup<NativeFunction<Void Function(Int64)>>('setDartApiMessagePort').asFunction();
+        setDartApiMessagePort(nativePort);
 
         return true;
     }
