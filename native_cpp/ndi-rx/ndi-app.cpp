c5b5826d5865db8277d609357035c031752ac513
Iaroslav Dochien <iaroslav@atomos.com>
2023-03-15 15:16:44 +1100

    Added code for NDI Program RX

---
+++ native_cpp/ndi-rx/ndi-app.cpp
@@ -0,0 +1,165 @@
+#include "ndi-app.hpp"
+
+#include <iostream>
+#include <type_traits>
+
+NdiApp::NdiApp()
+{
+}
+
+NdiApp::~NdiApp()
+{
+}
+
+
+bool NdiApp::createReceiver(const std::string& name, const std::string& url, Quality quality)
+{
+    const NDIlib_source_t ndiSources(name.c_str(), url.c_str());
+
+    // TODO: use NDIlib_recv_color_format_compressed_v5 to pass compressed video through without automatic decompression
+    NDIlib_recv_create_v3_t recvDescHi{};
+    recvDescHi.color_format = NDIlib_recv_color_format_fastest;
+    recvDescHi.bandwidth = (quality == Quality::High) ? NDIlib_recv_bandwidth_highest : NDIlib_recv_bandwidth_lowest;
+    recvDescHi.allow_video_fields = true;
+
+    mRecvInst.reset();
+    std::shared_ptr<RecvClass> rxInst = std::make_shared<RecvClass>(recvDescHi);
+    if (!rxInst)
+    {
+        return false;
+    }
+    mRecvInst = rxInst;
+
+    // Connect to our sources
+    NDIlib_recv_connect(mRecvInst->src(), &ndiSources);
+
+    return true;
+}
+
+bool NdiApp::capturePackets()
+{
+    if (!mRecvInst)
+    {
+        return false;
+    }
+
+    return captureBlock(mRecvInst);
+}
+
+bool NdiApp::captureBlock(std::shared_ptr<RecvClass> rxInst)
+{
+    NDIlib_video_frame_v2_t* video = (NDIlib_video_frame_v2_t*)malloc(sizeof(NDIlib_video_frame_v2_t));
+    NDIlib_audio_frame_v3_t* audio = (NDIlib_audio_frame_v3_t*)malloc(sizeof(NDIlib_audio_frame_v3_t));
+    NDIlib_metadata_frame_t* meta = (NDIlib_metadata_frame_t*)malloc(sizeof(NDIlib_metadata_frame_t));
+
+    NDIlib_frame_type_e ret = NDIlib_recv_capture_v3(rxInst->src(), video, audio, meta, 1000);
+
+    switch (ret)
+    {
+        case NDIlib_frame_type_none:
+            free(video);
+            free(audio);
+            free(meta);
+            break;
+
+        case NDIlib_frame_type_video:
+        {
+            if (video->p_data)
+            {
+                // push video to queue after checking for audio !!!
+                auto releaseCb = [this, rxInst](void* userData)
+                {
+                    // rxInst is owned by class object
+                    if (!userData) { return; }
+                    auto video = (NDIlib_video_frame_v2_t*)userData;
+                    NDIlib_recv_free_video_v2(rxInst->src(), video);
+                    free(video);
+                };
+                receivedPack(video, releaseCb);
+                free(audio);
+            }
+            else
+            {
+                NDIlib_recv_free_video_v2(rxInst->src(), video);
+                free(video);
+                free(audio);
+            }
+            free(meta);
+            break;
+        }
+
+        case NDIlib_frame_type_audio:
+        {
+            auto releaseCb = [this, rxInst](void* userData)
+            {
+                // rxInst is owned by class object
+                if (!userData) { return; }
+                auto audio = (NDIlib_audio_frame_v3_t*)userData;
+                NDIlib_recv_free_audio_v3(rxInst->src(), audio);
+                free(audio);
+            };
+            receivedPack(audio, releaseCb);
+            free(video);
+            free(meta);
+            break;
+        }
+
+        case NDIlib_frame_type_metadata:
+            NDIlib_recv_free_metadata(rxInst->src(), meta);
+            free(video);
+            free(audio);
+            free(meta);
+            break;
+
+        case NDIlib_frame_type_error:
+            std::cerr << "NDI pack rx error" << std::endl;
+            break;
+
+        default:
+            free(video);
+            free(audio);
+            free(meta);
+            break;
+    }
+    return true;
+}
+
+void NdiApp::addObserver(InputPacketsObserver* obs)
+{
+    if (obs)
+    {
+        auto iter = mInputPacketsObservers.find(obs);
+        if (iter == mInputPacketsObservers.cend())
+        {
+            mInputPacketsObservers.emplace(obs);
+        }
+    }
+}
+
+void NdiApp::removeObserver(InputPacketsObserver* obs)
+{
+    if (obs)
+    {
+        auto iter = mInputPacketsObservers.find(obs);
+        if (iter != mInputPacketsObservers.cend())
+        {
+            mInputPacketsObservers.erase(iter);
+        }
+    }
+}
+
+template <typename T>
+void NdiApp::receivedPack(T *pack, std::function<void(void* userData)> releaseCb)
+{
+    for (auto& el: mInputPacketsObservers)
+    {
+        if constexpr (std::is_same_v<T, NDIlib_video_frame_v2_t>)
+        {
+            el->receivedVideoPack(pack, releaseCb);
+        }
+        else if constexpr (std::is_same_v<T, NDIlib_video_frame_v2_t>)
+        {
+            el->receivedAudioPack(pack, releaseCb);
+        }
+    }
+}
---
+++ native_cpp/ndi-rx/ndi-app.hpp
@@ -0,0 +1,96 @@
+
+#pragma once
+
+#include <cstddef>
+
+#include </home/iaroslav/work/streaming/NDI/5.5.3/NDI-Advanced-SDK-for-Linux/include/Processing.NDI.Advanced.h>
+
+#include "interfaces-ndi/input_packet_observer.hpp"
+
+#include <string>
+#include <memory>
+#include <set>
+#include <functional>
+
+class NdiApp
+{
+public:
+    enum class Quality
+    {
+        Low,
+        High
+    };
+
+    NdiApp();
+    ~NdiApp();
+
+    bool createReceiver(const std::string& name, const std::string& url, Quality quality);
+    bool capturePackets();
+
+    void addObserver(InputPacketsObserver* obs);
+    void removeObserver(InputPacketsObserver* obs);
+
+private:
+    class RecvClass
+    {
+    public:
+        RecvClass(const NDIlib_recv_create_v3_t& arg)
+        {
+#if 1
+            const char* ndiConfig = nullptr;
+#else
+            const char* ndiConfig = R"({
+                "ndi": {
+                    "rudp": { "recv": {"enable": true}},
+                    "tcp": { "recv": {"enable": false}}
+                }
+            })";
+#endif
+
+            mVal = NDIlib_recv_create_v4(&arg, ndiConfig);
+        }
+        RecvClass(const RecvClass&) = delete;
+        RecvClass& operator=(const RecvClass&) = delete;
+        RecvClass(RecvClass&& rhs)
+        {
+            mVal = rhs.mVal;
+            rhs.mVal = nullptr;
+        }
+        RecvClass& operator=(RecvClass&& rhs)
+        {
+            mVal = rhs.mVal;
+            rhs.mVal = nullptr;
+            return *this;
+        }
+        ~RecvClass()
+        {
+            if (mVal)
+            {
+                NDIlib_recv_destroy(mVal);
+            }
+        }
+        NDIlib_recv_instance_t operator()()
+        {
+            return mVal;
+        }
+        NDIlib_recv_instance_t src()
+        {
+            return mVal;
+        }
+        operator bool() const
+        {
+            return mVal != nullptr;
+        }
+
+    private:
+        NDIlib_recv_instance_t mVal = nullptr;
+    };
+
+    std::shared_ptr<RecvClass> mRecvInst;
+
+    bool captureBlock(std::shared_ptr<RecvClass> rxInst); // a blocking function
+
+    template<typename T>
+    void receivedPack(T *pack, std::function<void(void* userData)>);
+    std::set<InputPacketsObserver*> mInputPacketsObservers;
+};
