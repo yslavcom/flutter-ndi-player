67cc7f04d803b0c672d64ddd2d42f8dbf89dd78e
Iaroslav Dochien <iaroslav@atomos.com>
2023-04-02 10:55:58 +1000

    Log builds & runs under linux, build fails on android

--- native_cpp/common/logger.cpp
+++ native_cpp/common/logger.cpp
@@ -1,1 +1,39 @@
 #include "logger.hpp"
+
+/////////////////////////////////////////////
+#if ANDROID_OUT
+
+#include <android/log.h>
+
+// void LOGV(const char * format, ... ) { __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG,__VA_ARGS__); }
+// void LOGD(const char * format, ... ) { __android_log_print(ANDROID_LOG_DEBUG  , LOG_TAG,__VA_ARGS__); }
+// void LOGI(const char * format, ... ) { __android_log_print(ANDROID_LOG_INFO   , LOG_TAG,__VA_ARGS__); }
+void LOGW(const char * format, ... ) { __android_log_print(ANDROID_LOG_WARN   , LOG_TAG,__VA_ARGS__); }
+void LOGE(const char * format, ... ) { __android_log_print(ANDROID_LOG_ERROR  , LOG_TAG,__VA_ARGS__); }
+// void LOGSIMPLE(const char * format, ... ) {}
+#endif
+
+/////////////////////////////////////////////
+#ifdef LINUX_OUT
+
+void LOGW(const char * format, ... )
+{
+    printf("  * Warning: ");
+
+    va_list arg_list;
+    va_start(arg_list, format);
+    vfprintf(stderr, format, arg_list);
+    va_end(arg_list);
+}
+
+
+void LOGE(const char * format, ... )
+{
+    printf("  *** Error:  ");
+    va_list arg_list;
+    va_start(arg_list, format);
+    vfprintf(stderr, format, arg_list);
+    va_end(arg_list);
+}
+
+#endif
--- native_cpp/common/logger.hpp
+++ native_cpp/common/logger.hpp
@@ -1,28 +1,14 @@
-/////////////////////////////////////////////
-#if ANDROID_OUT
+#pragma once
 
-#include <android/log.h>
-
-    // LOGS ANDROID
-#include <android/log.h>
-#define LOG_TAG ""
-#define LOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG,__VA_ARGS__)
-#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG  , LOG_TAG,__VA_ARGS__)
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO   , LOG_TAG,__VA_ARGS__)
-#define LOGW(...) __android_log_print(ANDROID_LOG_WARN   , LOG_TAG,__VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR  , LOG_TAG,__VA_ARGS__)
-#define LOGSIMPLE(...)
-
-#endif
-/////////////////////////////////////////////
-#if LINUX_OUT
-
+#include <stdarg.h>
 #include <stdio.h>
-#define LOGV(...) printf("  ");printf(__VA_ARGS__);
-#define LOGD(...) printf("  ");printf(__VA_ARGS__);
-#define LOGI(...) printf("  ");printf(__VA_ARGS__);
-#define LOGW(...) printf("  * Warning: "); printf(__VA_ARGS__);
-#define LOGE(...) printf("  *** Error:  ");printf(__VA_ARGS__);
-#define LOGSIMPLE(...) printf(" ");
+#ifdef __cplusplus
+extern "C" {
+#endif
 
+void LOGW(const char * format, ... );
+void LOGE(const char * format, ... );
+
+#ifdef __cplusplus
+}
 #endif
