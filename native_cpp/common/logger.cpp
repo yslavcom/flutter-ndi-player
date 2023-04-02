6e241fc2d8d2a8402bb6dd4b4f1e2dfd91cf6605
Iaroslav Dochien <iaroslav@atomos.com>
2023-04-02 11:06:28 +1000

    Log builds under android

--- native_cpp/common/logger.cpp
+++ native_cpp/common/logger.cpp
@@ -4,12 +4,33 @@
 #if ANDROID_OUT
 
 #include <android/log.h>
+#include <stdarg.h>
+
+#define LOG_TAG ""
 
 // void LOGV(const char * format, ... ) { __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG,__VA_ARGS__); }
 // void LOGD(const char * format, ... ) { __android_log_print(ANDROID_LOG_DEBUG  , LOG_TAG,__VA_ARGS__); }
 // void LOGI(const char * format, ... ) { __android_log_print(ANDROID_LOG_INFO   , LOG_TAG,__VA_ARGS__); }
-void LOGW(const char * format, ... ) { __android_log_print(ANDROID_LOG_WARN   , LOG_TAG,__VA_ARGS__); }
-void LOGE(const char * format, ... ) { __android_log_print(ANDROID_LOG_ERROR  , LOG_TAG,__VA_ARGS__); }
+void LOGW(const char * format, ... )
+{
+    va_list arg;
+    va_start(arg, format);
+    char str[256];
+    vsnprintf(str, 256, format, arg);
+    __android_log_print(ANDROID_LOG_WARN, LOG_TAG, "%s: %s", str);
+    va_end(arg);
+}
+
+void LOGE(const char * format, ... )
+{
+    va_list arg;
+    va_start(arg, format);
+    char str[256];
+    vsnprintf(str, 256, format, arg);
+    __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, "%s: %s", str);
+    va_end(arg);
+}
+
 // void LOGSIMPLE(const char * format, ... ) {}
 #endif
 
