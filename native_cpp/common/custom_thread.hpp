9db8262e20ebacd8c5494bc331db973b2875bba6
Iaroslav Dochien <iaroslav@atomos.com>
2023-06-14 21:13:58 +1000

    Added Custom Thread class

---
+++ native_cpp/common/custom_thread.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "common/logger.hpp"
+
+#include <thread>
+
+class CustomThread final
+{
+public:
+    ~CustomThread()
+    {
+        terminate();
+    }
+
+    template <typename Callback, typename... Args>
+    bool start(Callback cb, Args ...arg)
+    {
+        if (mThread.joinable())
+        {
+            mStop = true;
+            mThread.join();
+        }
+
+        mStop = false;
+        if (!mThread.joinable())
+        {
+            auto lambda = [this](Callback cb, Args... arg){
+                while(!mStop)
+                {
+                    cb(mStop, arg...);
+                }
+            };
+
+            mThread = std::thread(lambda, cb, arg...);
+        }
+
+        return mThread.joinable();
+    }
+
+    bool terminate()
+    {
+        if (mThread.joinable())
+        {
+            mStop = true;
+            mThread.join();
+        }
+        return !mThread.joinable();
+    }
+
+private:
+    std::thread mThread;
+    std::atomic<bool> mStop{false};
+};
+
