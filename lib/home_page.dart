d2e59fb161334d5346ce3feffba65a56a3b7bbb1
Iaroslav Dochien <iaroslav@atomos.com>
2023-11-22 20:11:29 +1100

    Removed `Andrdoi Debug` window and removed dart player window for linux

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,7 +8,8 @@ import 'my_app_state.dart';
 import 'sources_page.dart';
 import 'program_control.dart';
 import 'player_texture.dart';
-import 'android_debug.dart';
+
+import 'dart:io';
 
 class MyHomePage extends StatefulWidget {
     const MyHomePage({super.key});
@@ -31,61 +32,99 @@ class _MyHomePageState extends State<MyHomePage> {
                 page = SourcesPage();
                 break;
             case PageIndex.player:
-                page = PlayerTex();
-                break;
-            case PageIndex.androidDebug:
-                page = AndroidDebug();
+                page = const PlayerTex();
                 break;
             default:
                 throw UnimplementedError('no widget for $selectedPage');
         }
 
-        return LayoutBuilder(
-            builder: (context, constraints) {
-                return Scaffold(
-                    body: Row(
-                        children: [
-                            SafeArea(
-                                child: NavigationRail(
-                                    extended: constraints.maxWidth >= 600,
-                                    destinations: const [
-                                        NavigationRailDestination(
-                                            icon: Icon(Icons.home),
-                                            label: Text('Home'),
-                                        ),
-                                        NavigationRailDestination(
-                                            icon: Icon(Icons.list),
-                                            label: Text('Sources'),
-                                        ),
-                                        NavigationRailDestination(
-                                            icon: Icon(Icons.abc),
-                                            label: Text('Player'),
-                                        ),
-                                        NavigationRailDestination(
-                                            icon: Icon(Icons.access_alarm),
-                                            label: Text('Android Debug'),
-                                        ),
-                                    ],
-                                    selectedIndex: selectedPage.index,
-                                    onDestinationSelected: (value) {
-                                        setState(() {
-                                          selectedPage = PageIndex.values[value];
-                                        });
-                                    }
-                                    )
-                            ),
-                            // Display the page on the right handside
-                            Expanded(
-                                    child: Container(
-                                    color: Theme.of(context).colorScheme.primaryContainer,
-                                    child: page,
-                                ),
-                            ),
-                        ],
-                    )
-                );
-            }
-        );
+        if (Platform.isAndroid)
+        {
+          return LayoutBuilder(
+              builder: (context, constraints) {
+                  return Scaffold(
+                      body: Row(
+                          children: [
+                              SafeArea(
+                                  child: NavigationRail(
+                                      extended: constraints.maxWidth >= 600,
+                                      destinations: const [
+                                          NavigationRailDestination(
+                                              icon: Icon(Icons.home),
+                                              label: Text('Home'),
+                                          ),
+                                          NavigationRailDestination(
+                                              icon: Icon(Icons.list),
+                                              label: Text('Sources'),
+                                          ),
+                                          NavigationRailDestination(
+                                              icon: Icon(Icons.abc),
+                                              label: Text('Player'),
+                                          ),
+                                      ],
+                                      selectedIndex: selectedPage.index,
+                                      onDestinationSelected: (value) {
+                                          setState(() {
+                                            selectedPage = PageIndex.values[value];
+                                          });
+                                      }
+                                      )
+                              ),
+                              // Display the page on the right handside
+                              Expanded(
+                                      child: Container(
+                                      color: Theme.of(context).colorScheme.primaryContainer,
+                                      child: page,
+                                  ),
+                              ),
+                          ],
+                      )
+                  );
+              }
+          );
+        }
+        else
+        {
+          // Linux code
+          return LayoutBuilder(
+              builder: (context, constraints) {
+                  return Scaffold(
+                      body: Row(
+                          children: [
+                              SafeArea(
+                                  child: NavigationRail(
+                                      extended: constraints.maxWidth >= 600,
+                                      destinations: const [
+                                          NavigationRailDestination(
+                                              icon: Icon(Icons.home),
+                                              label: Text('Home'),
+                                          ),
+                                          NavigationRailDestination(
+                                              icon: Icon(Icons.list),
+                                              label: Text('Sources'),
+                                          ),
+                                      ],
+                                      selectedIndex: selectedPage.index,
+                                      onDestinationSelected: (value) {
+                                          setState(() {
+                                            selectedPage = PageIndex.values[value];
+                                          });
+                                      }
+                                      )
+                              ),
+                              // Display the page on the right handside
+                              Expanded(
+                                      child: Container(
+                                      color: Theme.of(context).colorScheme.primaryContainer,
+                                      child: page,
+                                  ),
+                              ),
+                          ],
+                      )
+                  );
+              }
+          );
+        }
     }
 }
 
