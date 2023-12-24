c04d6ee96250bb296cb58e99694539a028a59688
Iaroslav Dochien <iaroslav@atomos.com>
2023-12-24 19:54:16 +1100

    Removed the player page`

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,129 +1,109 @@
+import 'pages.dart';
+import 'program_control.dart';
+import 'texture_controller.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
-import 'pages.dart';
-import 'program_control.dart';
-import 'player_texture.dart';
-
 import 'dart:io';
 
 class MyHomePage extends StatefulWidget {
-    const MyHomePage({super.key});
-    @override
-    State<MyHomePage> createState() => _MyHomePageState();
+  const MyHomePage({super.key});
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-    PageIndex selectedPage = PageIndex.home;
+  PageIndex selectedPage = PageIndex.home;
 
-    @override
-    initState() {
-      super.initState();
-      SystemChrome.setPreferredOrientations([
-        DeviceOrientation.landscapeRight,
-        DeviceOrientation.landscapeLeft,
-      ]);
+  @override
+  initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+    ]);
 
-      ProgramControl().scanPrograms();
+    ProgramControl().scanPrograms();
+  }
+
+  Widget homePage = HomePage();
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page = homePage;
+    switch (selectedPage) {
+      case PageIndex.home:
+        page = HomePage();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedPage');
     }
 
-    Widget homePage = HomePage();
-
-    @override
-    Widget build(BuildContext context) {
-        Widget page = homePage;
-        switch(selectedPage) {
-            case PageIndex.home:
-                page = HomePage();
-                break;
-            case PageIndex.player:
-                page = const PlayerTex();
-                break;
-            default:
-                throw UnimplementedError('no widget for $selectedPage');
-        }
-
-        if (Platform.isAndroid)
-        {
-          return LayoutBuilder(
-              builder: (context, constraints) {
-                  return Scaffold(
-                      body: Row(
-                          children: [
-                              SafeArea(
-                                  child: NavigationRail(
-                                      extended: constraints.maxWidth >= 600,
-                                      destinations: const [
-                                          NavigationRailDestination(
-                                              icon: Icon(Icons.home),
-                                              label: Text('Home'),
-                                          ),
-                                          NavigationRailDestination(
-                                              icon: Icon(Icons.abc),
-                                              label: Text('Player'),
-                                          ),
-                                      ],
-                                      selectedIndex: selectedPage.index,
-                                      onDestinationSelected: (value) {
-                                          setState(() {
-                                            selectedPage = PageIndex.values[value];
-                                          });
-                                      }
-                                      )
-                              ),
-                              // Display the page on the right handside
-                              Expanded(
-                                      child: Container(
-                                      color: Theme.of(context).colorScheme.primaryContainer,
-                                      child: page,
-                                  ),
-                              ),
-                          ],
-                      )
-                  );
-              }
-          );
-        }
-        else
-        {
-          // Linux code
-          return LayoutBuilder(
-              builder: (context, constraints) {
-                  return Scaffold(
-                      body: Row(
-                          children: [
-                              SafeArea(
-                                  child: NavigationRail(
-                                      extended: constraints.maxWidth >= 600,
-                                      destinations: const [
-                                          NavigationRailDestination(
-                                              icon: Icon(Icons.home),
-                                              label: Text('Home'),
-                                          ),
-                                      ],
-                                      selectedIndex: selectedPage.index,
-                                      onDestinationSelected: (value) {
-                                          setState(() {
-                                            selectedPage = PageIndex.values[value];
-                                          });
-                                      }
-                                      )
-                              ),
-                              // Display the page on the right handside
-                              Expanded(
-                                      child: Container(
-                                      color: Theme.of(context).colorScheme.primaryContainer,
-                                      child: page,
-                                  ),
-                              ),
-                          ],
-                      )
-                  );
-              }
-          );
-        }
+    if (Platform.isAndroid) {
+      return LayoutBuilder(builder: (context, constraints) {
+        return Scaffold(
+            body: Row(
+          children: [
+            SafeArea(
+                child: NavigationRail(
+                    extended: constraints.maxWidth >= 600,
+                    destinations: const [
+                      NavigationRailDestination(
+                        icon: Icon(Icons.home),
+                        label: Text('Home'),
+                      ),
+                    ],
+                    selectedIndex: selectedPage.index,
+                    onDestinationSelected: (value) {
+                      setState(() {
+                        selectedPage = PageIndex.values[value];
+                      });
+                    })),
+            // Display the page on the right handside
+            Expanded(
+              child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: page,
+              ),
+            ),
+          ],
+        ));
+      });
+    } else {
+      // Linux code
+      return LayoutBuilder(builder: (context, constraints) {
+        return Scaffold(
+            body: Row(
+          children: [
+            SafeArea(
+                child: NavigationRail(
+                    extended: constraints.maxWidth >= 600,
+                    destinations: const [
+                      NavigationRailDestination(
+                        icon: Icon(Icons.home),
+                        label: Text('Home'),
+                      ),
+                    ],
+                    selectedIndex: selectedPage.index,
+                    onDestinationSelected: (value) {
+                      setState(() {
+                        selectedPage = PageIndex.values[value];
+                      });
+                    })),
+            // Display the page on the right handside
+            Expanded(
+              child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: page,
+              ),
+            ),
+          ],
+        ));
+      });
     }
+  }
 }
 
 class HomePage extends StatefulWidget {
@@ -131,55 +111,66 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>
-{
+class _HomePageState extends State<HomePage> {
   List<String> programNames = [];
+
+  // Texture controller
+  final _controller = OpenGLTextureController();
+  final _width = 1920;
+  final _height = 1080;
 
   @override
   initState() {
     super.initState();
-
     ProgramControl().registerListUpdateCallback(updateListView);
 
-    if (kDebugMode)
-    {
-      print('programNames:$programNames');
-    }
+    initializeController();
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     var theme = Theme.of(context);
 
+    // Program names list
     programNames = ProgramControl().getProgramsName();
-
-    if (kDebugMode)
-    {
-      print('list builder');
-    }
-
     ProgramControl().scanPrograms();
+
+    // Build
     return Scaffold(
         backgroundColor: theme.colorScheme.background,
-        body: ListView.separated(
-            itemCount: programNames.length,
-            separatorBuilder: (BuildContext context, int index) => Divider(
-              height: 1,
-              color: theme.colorScheme.surfaceVariant,
-              thickness: 1,
-            ),
-            itemBuilder: (BuildContext context, int index) => ListTile(
-              textColor: theme.colorScheme.primary,
-              title: Text(programNames[index]),
-              onLongPress: () {
-                  ProgramControl().startProgram(index);
-              },
-              onTap: (){
-                  ProgramControl().startProgram(index);
-              },
-            ),
-        ),
-      );
+        body: Stack(children: [
+          Opacity(
+              opacity: 0.5,
+              child: Center(
+                child: SizedBox(
+                  width: _width.toDouble(),
+                  height: _height.toDouble(),
+                  child: _controller.isInitialized
+                      ? Texture(textureId: _controller.textureId)
+                      : null,
+                ),
+              )),
+          Opacity(
+              opacity: 1.0,
+              child: ListView.separated(
+                itemCount: programNames.length,
+                separatorBuilder: (BuildContext context, int index) => Divider(
+                  height: 1,
+                  color: theme.colorScheme.surfaceVariant,
+                  thickness: 1,
+                ),
+                itemBuilder: (BuildContext context, int index) => ListTile(
+                  textColor: theme.colorScheme.primary,
+                  title: Text(programNames[index]),
+                  onLongPress: () {
+                    ProgramControl().startProgram(index);
+                  },
+                  onTap: () {
+                    ProgramControl().startProgram(index);
+                  },
+                ),
+              )),
+        ]));
   }
 
   // Callback function to update the ListView
@@ -190,5 +181,10 @@ class _HomePageState extends State<HomePage>
         print('NDI inputs list:$programNames');
       }
     });
+  }
+
+  Future<void> initializeController() async {
+    await _controller.initialize(_width, _height);
+    setState(() {});
   }
 }
--- lib/pages.dart
+++ lib/pages.dart
@@ -1,5 +1,5 @@
 enum PageIndex {
     home,
     // sources,
-    player,
+    // player,
 }
